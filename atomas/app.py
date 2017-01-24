20ab6d8976b977b3d0f1d055f5cca16546b55085
Denys Duchier <denys.duchier@univ-orleans.fr>
2017-01-24 19:49:33 +0100

    create app

---
+++ atomas/app.py
@@ -0,0 +1,6 @@
+from kivy.app import App
+
+class AtomasApp(App):
+
+    def on_pause(self):
+        return True
---
+++ main.py
@@ -0,0 +1,6 @@
+__version__ = '0.1.0'
+
+from atomas import AtomasApp
+
+zapp = AtomasApp()
+zapp.run()
